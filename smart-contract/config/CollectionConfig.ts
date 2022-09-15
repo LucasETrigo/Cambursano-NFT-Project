90c7352ab5da470eccdd774fdd63a9446fc161a7
Lucas Trigo <lucasetrigo@gmail.com>
2022-09-15 16:32:27 -0300

    Smart Contract + Minting DApp(React)

---
+++ smart-contract/config/CollectionConfig.ts
@@ -0,0 +1,35 @@
+import CollectionConfigInterface from '../lib/CollectionConfigInterface';
+import { ethereumTestnet, ethereumMainnet } from '../lib/Networks';
+import { openSea } from '../lib/Marketplaces';
+import whitelistAddresses from './whitelist.json';
+
+const CollectionConfig: CollectionConfigInterface = {
+  testnet: ethereumTestnet,
+  mainnet: ethereumMainnet,
+  // The contract name can be updated using the following command:
+  // yarn rename-contract NEW_CONTRACT_NAME
+  // Please DO NOT change it manually!
+  contractName: 'Cambursano',
+  tokenName: 'Cambursano Monos',
+  tokenSymbol: 'CAMBs',
+  hiddenMetadataUri: 'ipfs://Qmc9M3qG6UaGcFexBWSvJt8Rz1PLYt6N4t3L5GgsFDVXnP/hidden.json',
+  maxSupply: 35,
+  whitelistSale: {
+    price: 0.05,
+    maxMintAmountPerTx: 1,
+  },
+  preSale: {
+    price: 0.07,
+    maxMintAmountPerTx: 2,
+  },
+  publicSale: {
+    price: 0.09,
+    maxMintAmountPerTx: 5,
+  },
+  contractAddress: "0x8b23CD4c661ce1fa0331927594f5DC349Da6A03f",
+  marketplaceIdentifier: 'my-nft-token',
+  marketplaceConfig: openSea,
+  whitelistAddresses: whitelistAddresses,
+};
+
+export default CollectionConfig;
